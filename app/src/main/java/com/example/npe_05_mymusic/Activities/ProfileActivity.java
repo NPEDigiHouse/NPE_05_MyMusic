32b395eff1f429451d513323aadff43a200a31dd
さん <phantom26isn@gmail.com>
2021-05-29 17:38:51 +0800

    Add Some

--- app/src/main/java/com/example/npe_05_mymusic/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/npe_05_mymusic/Activities/ProfileActivity.java
@@ -6,17 +6,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.npe_05_mymusic.R;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnAdd;
+    ImageButton btnBack;
+    TextView tvEditProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        // Find View
         btnAdd = findViewById(R.id.btn_add);
+        btnBack = findViewById(R.id.ib_back);
+        tvEditProfile = findViewById(R.id.tv_edit);
+        // Onclick Listner
         btnAdd.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        tvEditProfile.setOnClickListener(this);
     }
 
     @Override
@@ -26,6 +36,15 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 Intent i = new Intent(ProfileActivity.this, UploadActivity.class);
                 startActivity(i);
                 break;
+            case R.id.tv_edit:
+                Intent editProfileIntent = new Intent(ProfileActivity.this, EditProfileActivity.class);
+                startActivity(editProfileIntent);
+                break;
+            case R.id.ib_back:
+                Intent back = new Intent(ProfileActivity.this, MainActivity.class);
+                startActivity(back);
+                break;
+
         }
     }
 }
